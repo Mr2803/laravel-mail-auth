384ce35788049b0a5fdb5b435cb9e5ddb9bcae14
Mr2803 <rispoli.mar2803@gmail.com>
2020-01-23 15:05:42 +0100

    fix code , add requests

--- public/js/app.js
+++ public/js/app.js
@@ -49661,8 +49661,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\rispo\OneDrive\Desktop\Boolean\Laravel\Prog23-01\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\rispo\OneDrive\Desktop\Boolean\Laravel\Prog23-01\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\rispo\OneDrive\Desktop\Boolean\Laravel\laravel-mail-auth\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\rispo\OneDrive\Desktop\Boolean\Laravel\laravel-mail-auth\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
